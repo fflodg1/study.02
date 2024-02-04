bda2c23b9892dc62807bf22fae2021e513207598
fflodg1 <diglkoor@gmail.com>
2024-02-04 20:50:50 +0600

    'first'

---
+++ lesson.2_1.py
@@ -0,0 +1,30 @@
+# Концепция ООП
+
+# imort random, math, turtle
+
+
+class Car:
+    # атрибуты или свойства класса
+    motor=True
+    color='black'
+    # функция внутри класса называется - метод класса
+#     конструткор класса
+    def __init__(self, name):
+        self.age=age
+        self.name=name
+
+# экземпляр либо объект класса
+bmw=Car('bmw', 2020)
+mers=Car('benz', 2023)
+mers.color='black'
+print(type(mers))
+print(mers.motor, mers.color)
+print(bmw.motor, bmw.color)
+
+# def a():
+#     a()
+# class A:
+#     pass
+#
+# p=2.1, 1 '1', True, [], {}, (), None
+# print(type(p))
---
+++ lesson.2_2.py
@@ -0,0 +1,1 @@
+# git remote add origin https://github.com/fflodg1/study.02.git
